c4ccb97faa217ae46b9ca952c772fa2300406791
lawal <sphade010@gmail.com>
2022-12-30 00:25:26 +0100

    completed setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BuyerDashboardLayout from "./layout/BuyerDashboardLayout";
 import Login from "./pages/buyer/auth/Login";
+import DisputeResolution from "./pages/buyer/dashboard/DisputeResolution";
 import Home from "./pages/buyer/dashboard/Home";
-
-
+import Settings from "./pages/buyer/dashboard/Settings";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BuyerDashboardLayout />}>
+        <Route path="/buyer" element={<BuyerDashboardLayout />}>
           <Route index element={<Home />} />
+          <Route path="dispute-resolution" element={<DisputeResolution />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
-        <Route path="/login" element={ <Login/> }/>
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
