9f7f9c15a3230f5b928d730801471c46371e464b
Omobayode6 <omobayode93@gmail.com>
2023-05-03 11:16:48 +0100

    update code for building

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 //create an Axios instance with a config to prevent us from repeating these options in every request
-const BASE_URL = "ec2-3-83-206-99.compute-1.amazonaws.com/v1";
+const BASE_URL = "http://api.mybalanceapp.com/";
 
 export const authApi = axios.create({
   baseURL: BASE_URL,
