8be20dec6bdbafeedaa17c79139a813799f8d701
Omobayode6 <omobayode93@gmail.com>
2023-08-31 15:22:31 +0100

    building for production

--- src/pages/buyer/dashboard/Test.tsx
+++ src/pages/buyer/dashboard/Test.tsx
@@ -5,11 +5,11 @@ import ReactPaginate from "react-paginate";
 // Example items, to simulate fetching from another resources.
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-function Items({ currentItems }) {
+function Items({currentItems}:any) {
   return (
     <>
       {currentItems &&
-        currentItems.map((item) => (
+        currentItems.map((item:any) => (
           <div>
             <h3>Item #{item}</h3>
           </div>
@@ -18,7 +18,7 @@ function Items({ currentItems }) {
   );
 }
 
-export default function Test({ itemsPerPage }) {
+export default function Test({itemsPerPage}:any) {
   // Here we use item offsets; we could also use page offsets
   // following the API or data you're working with.
   const [itemOffset, setItemOffset] = useState(0);
@@ -32,7 +32,7 @@ export default function Test({ itemsPerPage }) {
   const pageCount = Math.ceil(items.length / itemsPerPage);
 
   // Invoke when user click to request another page.
-  const handlePageClick = (event) => {
+  const handlePageClick = (event:any) => {
     const newOffset = (event.selected * itemsPerPage) % items.length;
     console.log(
       `User requested page number ${event.selected}, which is offset ${newOffset}`
