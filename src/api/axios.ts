a5dd843e9fd8b24a2df8c0d7d9b3795da6b4a029
Tosin Ayodele <devtosxn@gmail.com>
2023-09-12 14:33:28 +0100

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 //create an Axios instance with a config to prevent us from repeating these options in every request
 // const BASE_URL = "http://ec2-3-86-147-94.compute-1.amazonaws.com/v1";
-const BASE_URL = "https://fastidious-bombolone-2dabe2.netlify.app/v1"
+const BASE_URL = "https://mybalanceapp.netlify.app/v1"
 
 export const publicApi = axios.create({
   baseURL: BASE_URL,
