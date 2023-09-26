9c5071ac77a7b30150815874477d42bad5c2e876
Omobayode6 <omobayode93@gmail.com>
2023-09-26 14:53:11 +0100

    Make the app responsive

--- src/layout/sellerSidebarDatas.tsx
+++ src/layout/sellerSidebarDatas.tsx
@@ -52,50 +52,50 @@ const sellerSidebarDatas = [
       </svg>
     ),
   },
-  {
-    title: "Notifications",
-    path: "/seller/notifications",
-    icon: (
-      <svg
-        className="w-6 h-6 stroke-current"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-      >
-        <g
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1.5"
-          clip-path="url(#a)"
-        >
-          <path d="M10 5.592a2 2 0 1 1 4 0 7 7 0 0 1 4 6v3a4 4 0 0 0 2 3H4a4 4 0 0 0 2-3v-3a7 7 0 0 1 4-6M9 17.592v1a3 3 0 0 0 6 0v-1" />
-        </g>
-        <defs>
-          <clipPath id="a">
-            <path fill="#fff" d="M0 .592h24v24H0z" />
-          </clipPath>
-        </defs>
-      </svg>
-    ),
-  },
-  {
-    title: "Customer support",
-    path: "/seller/customer-support",
-    icon: (
-      <svg
-        className="w-6 h-6 stroke-current"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
-        />
-      </svg>
-    ),
-  },
+  // {
+  //   title: "Notifications",
+  //   path: "/seller/notifications",
+  //   icon: (
+  //     <svg
+  //       className="w-6 h-6 stroke-current"
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       fill="none"
+  //     >
+  //       <g
+  //         stroke-linecap="round"
+  //         stroke-linejoin="round"
+  //         stroke-width="1.5"
+  //         clip-path="url(#a)"
+  //       >
+  //         <path d="M10 5.592a2 2 0 1 1 4 0 7 7 0 0 1 4 6v3a4 4 0 0 0 2 3H4a4 4 0 0 0 2-3v-3a7 7 0 0 1 4-6M9 17.592v1a3 3 0 0 0 6 0v-1" />
+  //       </g>
+  //       <defs>
+  //         <clipPath id="a">
+  //           <path fill="#fff" d="M0 .592h24v24H0z" />
+  //         </clipPath>
+  //       </defs>
+  //     </svg>
+  //   ),
+  // },
+  // {
+  //   title: "Customer support",
+  //   path: "/seller/customer-support",
+  //   icon: (
+  //     <svg
+  //       className="w-6 h-6 stroke-current"
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       fill="none"
+  //       viewBox="0 0 24 24"
+  //       stroke-width="1.5"
+  //     >
+  //       <path
+  //         stroke-linecap="round"
+  //         stroke-linejoin="round"
+  //         d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
+  //       />
+  //     </svg>
+  //   ),
+  // },
   {
     title: "My profile",
     path: "/seller/profile",
