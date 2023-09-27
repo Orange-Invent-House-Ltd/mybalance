5e9295a1e8f260316205fd739c1e61deea5c848b
lawal <sphade010@gmail.com>
2023-09-27 15:23:22 +0100

    update

--- src/components/sellers/SellerHeader.tsx
+++ src/components/sellers/SellerHeader.tsx
@@ -20,7 +20,7 @@ const Header = ({ Heading, Text }: HeaderProps) => {
   const [value, setValue] = useState("");
   const [accNum, setAccNum] = useState("");
 
-  const { handleSubmit, control, register } = useForm();
+  const { handleSubmit, control, reset } = useForm();
   const { data: banks, isLoading: bankIsLoading } = useBanks();
   const { data: user } = useUser();
   const {
@@ -50,6 +50,12 @@ const Header = ({ Heading, Text }: HeaderProps) => {
   const words = user?.fullName.split(" ");
   const firstLetter = words?.[0][0]; // Get the first letter of the first word
   const secondLetter = words?.[1] && words[1].length > 0 ? words[1][0] : "";
+  useEffect(() => {
+    reset({
+      accountNumber: user?.bankAccount.accountNumber,
+      accountName: user?.bankAccount.accountName,
+    });
+  })
   return (
     <div className="flex flex-col items-center md:flex-row gap-6 justify-between mb-8">
       <div className="flex gap-4">
@@ -100,98 +106,7 @@ const Header = ({ Heading, Text }: HeaderProps) => {
           </Button>
         </div>
       </div>
-      {/* {isVerify && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black-rgba flex justify-end z-50">
-          <div className="w-[400px] bg-white pl-[16px] pr-[34px] overflow-y-scroll">
-            <div className="flex items-start gap-4 mt-10 mb-4">
-              <img
-                src={back}
-                alt="back"
-                onClick={() => setIsVerify(false)}
-                className="mt-2 cursor-pointer"
-              />
-              <h6 className="text-[23px] font-medium">
-                Create One-Time MyBalance Link
-              </h6>
-            </div>
-            <p className="text-[16px] text-[#303030] font-normal mb-8">
-              The Item(s) Information should be filled by your prospective
-              buyers/customers.
-            </p>
-            <h1 className="text-[#EDEDED] text-lg font-medium">
-              ITEM(S) INFORMATION
-            </h1>
-            <div className="mt-6 flex flex-col gap-4">
-              <TextField
-                label="Purpose of escrow"
-                name="purpose"
-                placeholder="e.g 20,000"
-              />
-              <TextField
-                label="Type of item(s)"
-                name="item_type"
-                placeholder="****"
-              />
-              <TextField
-                label="Number of item(s)"
-                name="quantity"
-                placeholder="give a description"
-              />
-              <TextField
-                label="Amount"
-                name="amount"
-                placeholder="give a description"
-              />
-              <TextField
-                label="Delivery timeline"
-                name="timeline"
-                placeholder="Select number of days"
-              />
-            </div>
-            <h1 className="mt-6 text-[#EDEDED] text-lg font-medium">
-              VENDOR ACCOUNT INFORMATION
-            </h1>
-            <div className="mt-6 flex flex-col gap-4">
-              <TextField
-                label="Bank Name"
-                name="bank_name"
-                placeholder="Access Bank"
-              />
-              <TextField
-                label="Enter Account number"
-                name="account_number"
-                placeholder="1234567890"
-              />
-              <TextField
-                label="Account Name"
-                name="account_name"
-                placeholder="e.g JMusty Feet"
-              />
-              <TextField
-                label="Email"
-                name="email"
-                placeholder="e.g JMustyfeet@gmail.com"
-              />
-              <TextField
-                label="Phone number"
-                name="phone_number"
-                placeholder="090123456789"
-                value={value}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mt-6 mb-16">
-              <Button
-                disabled={value ? false : true}
-                fullWidth
-                onClick={() => setIsVerify(false)}
-              >
-                Share escrow link
-              </Button>
-            </div>
-          </div>
-        </div>
-      )} */}
+      
       <Dialog.Root open={isVerify}>
         <Dialog.Portal className="">
           <Dialog.Overlay
@@ -282,17 +197,13 @@ const Header = ({ Heading, Text }: HeaderProps) => {
                   </label>
                   <select
                     className="block border border-[#B7B7B7] w-full rounded-md p-2 outline-none focus:border-[#747373] "
-                    value={code}
-                    onChange={(e) => {
-                      setCode(e.target.value);
-                    }}
+                   disabled
                   >
-                    {banks?.data?.map((bank: any) => (
-                      <option key={bank.slug} value={bank.code}>
-                        {bank.name}
+                
+                      <option  >
+                      {user?.bankAccount?.bankName }
                       </option>
-                    ))}
-                    {bankIsLoading && <option value="">loading...</option>}
+                   
                   </select>
                 </div>
                 <TextField
@@ -300,23 +211,20 @@ const Header = ({ Heading, Text }: HeaderProps) => {
                   label="Enter Account number"
                   placeholder="1234567890"
                   name={"accountNumber"}
-                  onChange={(e) => {
-                    setAccNum(e.target.value);
-                  }}
-                  value={accNum}
+                
                 />
                 <div className="relative">
-                  {LookupIsLoading && <LoadingOverlay />}
+              
                   <TextField
                     readOnly={true}
                     control={control}
                     name={"accountName"}
                     label="Account Name"
-                    value={LookupData?.data.accountName}
+                    
                     placeholder="e.g JMusty Feet"
                   />
                 </div>
-                <TextField
+                {/* <TextField
                   control={control}
                   rules={{
                     required: "this field is required",
@@ -328,7 +236,7 @@ const Header = ({ Heading, Text }: HeaderProps) => {
                   name={"sellerEmail"}
                   label="Email Address"
                   placeholder="e.g JMustyfeet@gmail.com"
-                />
+                /> */}
               </div>
               <div className="mt-6 mb-16">
                 <Button
