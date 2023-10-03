7b497ae1b3e88e1afff0db6e0e4781d5318aa22a
Tosin Ayodele <devtosxn@gmail.com>
2023-10-03 08:56:12 +0100

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 //create an Axios instance with a config to prevent us from repeating these options in every request
 // const BASE_URL = import.meta.env.VITE_BASE_URL;
-const BASE_URL = 'https://staging-api.mybalanceapp.com/v1'
+const BASE_URL = 'https://api.mybalanceapp.com/v1'
 
 export const publicApi = axios.create({
   baseURL: BASE_URL,
