8592c354b49cbd6bbc95a2267d698706d4eb0b37
Omobayode6 <omobayode93@gmail.com>
2024-02-25 19:31:38 +0100

    Logout user onIdle

--- src/pages/buyer/index.tsx
+++ src/pages/buyer/index.tsx
@@ -16,23 +16,27 @@ import TransactionHistory from "./dashboard/TransactionHistory";
 import PageNotFound from "../home/PageNotFound";
 import AuthLayout from "../../layout/AuthLayout";
 import TransactionStatus from "./dashboard/TransactionStatus";
+import ProtectedRoutes from "../auth/PrivateRoutes";
 
 const Index = () => {
   return (
     <Routes>
-      <Route path="/" element={<BuyerDashboardLayout />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="quick-action" element={<QuickAction />} />
-        <Route path="transaction-history" element={<TransactionHistory />} />
-        <Route path="notifications" element={<Notifications />} />
-        <Route path="dispute-resolution" element={<DisputeResolution />} />
-        <Route path="dispute-resolution/add" element={<AddNewDispute />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="customer-support" element={<CustomerSupport />} />
-        <Route path="quick-action/lock" element={<LockNewAmount />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<BuyerDashboardLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="quick-action" element={<QuickAction />} />
+          <Route path="transaction-history" element={<TransactionHistory />} />
+          <Route path="notifications" element={<Notifications />} />
+          <Route path="dispute-resolution" element={<DisputeResolution />} />
+          <Route path="dispute-resolution/add" element={<AddNewDispute />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="customer-support" element={<CustomerSupport />} />
+          <Route path="quick-action/lock" element={<LockNewAmount />} />
+        </Route>
+        <Route path="payment-callback" element={<TransactionStatus />} />
       </Route>
-      <Route path="payment-callback" element={<TransactionStatus />} />
+      
       <Route path="" element={<AuthLayout />}>
         <Route path="/register" element={<Register />} />
       </Route>
