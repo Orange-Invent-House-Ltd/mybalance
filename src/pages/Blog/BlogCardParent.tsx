4ff2a5812f0b15e6644b44ddbc85d806025c0489
Ekanem-Iboro <eiboro@oinvent.com>
2024-03-12 14:27:09 +0100

    Add blog cards

--- src/pages/Blog/BlogCardParent.tsx
+++ src/pages/Blog/BlogCardParent.tsx
@@ -15,7 +15,7 @@ function BlogCardParent() {
 
   return (
     <div className=' transition-all duration-700 ease-in-out'>
-      <div className={`mt-[-170px] md:mt-[-200px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-7 gap-x-8 md:px-14 h-${expanded ? 'auto' : 'h'} overflow-${overflow} hover:cursor-pointer`}>
+      <div className={`mt-[-170px] md:mt-[-200px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-7 gap-x-8 md:px-14 h-${expanded ? 'full' : 'h'} overflow-${overflow} hover:cursor-pointer`}>
         {dummyData.map((cardData: CardData, index: number) => (
           <Card key={index} data={cardData} />
         ))}
