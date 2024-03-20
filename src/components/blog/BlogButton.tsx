654ea92d3076ad40289f8907cb1526db88a22430
Ekanem-Iboro <eiboro@oinvent.com>
2024-03-20 18:26:48 +0100

    Add blog cards

--- src/components/blog/BlogButton.tsx
+++ src/components/blog/BlogButton.tsx
@@ -1,16 +1,17 @@
-import { ArrowDown } from 'lucide-react';
-
+import React from 'react';
+import { ArrowDown, ArrowUp } from 'lucide-react';
 
 interface BlogButtonProps {
   onClick: () => void; // Define onClick prop type
+  expanded: boolean; // Define expanded prop type
 }
 
-function BlogButton({ onClick }: BlogButtonProps) {
+function BlogButton({ onClick, expanded }: BlogButtonProps) {
   return (
     <div className='flex justify-center items-center w-full mt-6'>
       <button onClick={onClick} className='flex justify-center items-center gap-1 bg-primary-blog text-primary-blogCol p-2 px-4 rounded-lg mt-10 mb-[-30px]'>
-        <span><ArrowDown size={25} /></span>
-        <span className=' text-sm font-bold'>Load more</span>
+        <span>{expanded ? <ArrowUp size={25} /> : <ArrowDown size={25} />}</span>
+        <span className='text-sm font-bold'>{expanded ? 'Show less' : 'Load more'}</span>
       </button>
     </div>
   );
--- src/components/blog/BlogCardParent.tsx
+++ src/components/blog/BlogCardParent.tsx
@@ -1,31 +1,38 @@
 import React, { useState } from 'react';
 import BlogButton from './BlogButton';
-import Card from './BlogCard'; 
-import user from '../../assets/images/user.jpg'
+import Card from './BlogCard';
+import user from '../../assets/images/user.jpg';
+import user2 from '../../assets/images/shirt_50.png';
+import { Link } from 'react-router-dom';
 
 function BlogCardParent() {
-  const [expanded, setExpanded] = useState(false); 
-  const [overflow, setOverflow] = useState('hidden'); 
+  const [expanded, setExpanded] = useState(false);
+  const [overflow, setOverflow] = useState('hidden');
 
   const toggleExpanded = () => {
     setExpanded(!expanded);
-    setOverflow(expanded ? 'hidden' : 'none'); 
+    setOverflow(expanded ? 'hidden' : 'visible');
   };
 
   return (
-    <div className=' transition-all duration-700 ease-in-out'>
-      <div className={`mt-[-170px] md:mt-[-200px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-7 gap-x-8 md:px-14 h-${expanded ? 'full' : 'h'} overflow-${overflow} hover:cursor-pointer`}>
+    <div className='transition-all duration-700 ease-in-out'>
+      <div className={`mt-[-170px] md:mt-[-200px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-y-7 gap-x-8 md:px-14 ${expanded ? 'h-full' : 'h-98'} overflow-${overflow} hover:cursor-pointer`}>
         {dummyDatas.map((dummyData, index) => (
-          <Card key={index} data={dummyData} />
+          <Link key={index} to={`/blog/${dummyData.id}/details`} state={{ blogData: dummyData }}>
+            <Card data={dummyData} state={{ blogData: dummyData }} />
+          </Link>
         ))}
       </div>
-      <BlogButton onClick={toggleExpanded} />
+      <BlogButton onClick={toggleExpanded} expanded={expanded} />
     </div>
   );
 }
 
+
+
 const dummyDatas = [
   {
+    id:1,
     imgs: user ,
     subheading:'Design',
     title: 'How collaboration makes us better designers',
@@ -36,6 +43,18 @@ const dummyDatas = [
 
   },
   {
+    id:2,
+    imgs: user2 ,
+    subheading:'Design',
+    title: 'How collaboration makes us better designers',
+    description: 'Collaboration can make our teams stronger, and our individual designs better.',
+    userprofile: user,
+    name:'Johny Raymond',
+    date:'20 Jan 2022'
+
+  },
+  {
+    id:3,
     imgs: user ,
     subheading:'Design',
     title: 'How collaboration makes us better designers',
@@ -46,6 +65,18 @@ const dummyDatas = [
 
   },
   {
+    id:4,
+    imgs: user2 ,
+    subheading:'Design',
+    title: 'How collaboration makes us better designers',
+    description: 'Collaboration can make our teams stronger, and our individual designs better.',
+    userprofile: user,
+    name:'Olivia Rhye',
+    date:'20 Jan 2022'
+
+  },
+  {
+    id:5,
     imgs: user ,
     subheading:'Design',
     title: 'How collaboration makes us better designers',
@@ -56,6 +87,18 @@ const dummyDatas = [
 
   },
   {
+    id:6,
+    imgs: user2 ,
+    subheading:'Design',
+    title: 'How collaboration makes us better designers',
+    description: 'Collaboration can make our teams stronger, and our individual designs better.',
+    userprofile: user,
+    name:'Olivia Rhye',
+    date:'20 Jan 2022'
+
+  },
+  {
+    id:7,
     imgs: user ,
     subheading:'Design',
     title: 'How collaboration makes us better designers',
@@ -66,7 +109,8 @@ const dummyDatas = [
 
   },
   {
-    imgs: user ,
+    id:8,
+    imgs: user2 ,
     subheading:'Design',
     title: 'How collaboration makes us better designers',
     description: 'Collaboration can make our teams stronger, and our individual designs better.',
@@ -76,36 +120,7 @@ const dummyDatas = [
 
   },
   {
-    imgs: user ,
-    subheading:'Design',
-    title: 'How collaboration makes us better designers',
-    description: 'Collaboration can make our teams stronger, and our individual designs better.',
-    userprofile: user,
-    name:'Olivia Rhye',
-    date:'20 Jan 2022'
-
-  },
-  {
-    imgs: user ,
-    subheading:'Design',
-    title: 'How collaboration makes us better designers',
-    description: 'Collaboration can make our teams stronger, and our individual designs better.',
-    userprofile: user,
-    name:'Olivia Rhye',
-    date:'20 Jan 2022'
-
-  },
-  {
-    imgs: user ,
-    subheading:'Design',
-    title: 'How collaboration makes us better designers',
-    description: 'Collaboration can make our teams stronger, and our individual designs better.',
-    userprofile: user,
-    name:'Olivia Rhye',
-    date:'20 Jan 2022'
-
-  },
-  {
+    id:9,
     imgs: user ,
     subheading:'Design',
     title: 'How collaboration makes us better designers',
