76f598ae2f50e10fe711add135401e55052e61f4
Omobayode6 <omobayode93@gmail.com>
2024-03-28 14:51:36 +0100

    test auto deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,19 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      /**
+       * Ignore "use client" waning since we are not using SSR
+       * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
+       */
+      onwarn(warning, warn) {
+        if (
+          warning.code === "MODULE_LEVEL_DIRECTIVE"
+        ) {
+          return;
+        }
+        warn(warning);
+      },
+    },
   },
 })
